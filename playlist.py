87342b78a2c01891777d7a7239b9a379f8c1e2d8
HadenLescallett <hlescallett@gmail.com>
2026-09-22 14:42:13 -0500

    Added Billie Jean to the playlist

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("Billie Jean", "Micheal Jackson"),
 ]
 
 def print_playlist(songs):
